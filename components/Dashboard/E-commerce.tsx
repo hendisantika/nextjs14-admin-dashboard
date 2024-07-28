5da186eecb0ad39c6981acd8cdcc86b9bfbdf91c
hendisantika <hendisantika@yahoo.co.id>
2024-07-28 08:15:31 +0700

    Bug Fix Error property :two: 🫸🌀✏️📗🐧🐳⬆

--- components/Dashboard/E-commerce.tsx
+++ components/Dashboard/E-commerce.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 
-import { AreaChart, SimpleBar } from "@/components/Charts";
 import ChatCard from "../Chat/ChatCard";
 import TableOne from "../Tables/TableOne";
 
@@ -22,8 +21,6 @@ const ECommerce: React.FC = () => {
         <DataCard name="customers" amount={400} />
       </div>
       <div className="space-y-5 py-5">
-        <AreaChart />
-        <SimpleBar />
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
