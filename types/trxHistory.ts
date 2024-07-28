050137da2766e59e15377d2618d3af57b812e1ae
hendisantika <hendisantika@yahoo.co.id>
2024-07-28 07:55:40 +0700

    Create NuxtJS 14 Project 🫸🌀✏️📗🐧🐳⬆

---
+++ types/trxHistory.ts
@@ -0,0 +1,17 @@
+export type TrxHistory = {
+  merchantID: string;
+  merchantMPAN: string;
+  merchantNMID: string;
+  amount: number;
+  billNumber: string;
+  expiryDuration: number;
+  invoiceNo: number;
+  DateTime: string;
+  RRN: string;
+  issuerName: string;
+  customerPAN: string;
+  customerName: string
+  mdr: string;
+  settlementDate: string;
+  trxStatus: string
+};
